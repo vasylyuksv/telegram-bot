9847a1d20d1918aaa2b476f7f8c557e1ae403c08
sv <vasylyuk@evergreens.in.ua>
2024-12-08 14:08:07 +0200

    first commit

--- src/bot.mjs
+++ src/bot.mjs
@@ -4,4 +4,12 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 bot.on("text", msg => msg.reply.text(msg.text))
 
+bot.on('/who', (msg) => {
+	return bot.sendMessage(msg.from.id, `Hello, ${msg.from.first_name}`)
+})
+
+bot.on('/start', (msg) =>
+	msg.reply.photo('https://duikt.edu.ua/img/logo_new.png')
+)
+
 export default bot
